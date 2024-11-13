bd5e69501a8efd09fb36b918bc4e8c7b10ddd575
Gabriel Key Shimada Matsuda <gabrielkey@usp.br>
2024-11-13 18:05:33 -0300

    Envio para github2

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-b10ck_mu)s4=vz!v%&gss0#rnwdg=nml_sgcz2a)fmx(@y*!3t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", ".onrender.com"] # modifique esta linha
+
+CSRF_TRUSTED_ORIGINS = ["https://*.onrender.com/"] # adicione esta linha
 
 
 # Application definition
@@ -48,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # adicione esta linha
 ]
 
 ROOT_URLCONF = 'blog_project.urls'
@@ -117,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # adicione esta linha
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
